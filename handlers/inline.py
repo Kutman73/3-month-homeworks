41c9dee904e26935aceb274d0dd10deffc4dd11b
Kutu73 <0.0000000002clash@gmail.com>
2022-11-02 17:17:43 +0300

    hw-7

---
+++ handlers/inline.py
@@ -0,0 +1,23 @@
+from aiogram import types, Dispatcher
+import hashlib
+
+
+# def finder(text):
+#     return YT(text, max_results=10).to_dict()
+
+
+async def inline_wiki_handler(query: types.InlineQuery):
+    text = query.query or "Python"
+    link = f'https://ru.wikipedia.org/wiki/{text}'
+    articles = [types.InlineQueryResultArticle(
+        id=hashlib.md5(text.encode()).hexdigest(),
+        title='WikiPedia',
+        url=link,
+        input_message_content=types.InputMessageContent(
+            message_text=f'Ссылка\n{link}'
+        ))]
+    await query.answer(articles, cache_time=60)
+
+
+def register_handler_inline(dp: Dispatcher):
+    dp.register_inline_handler(inline_wiki_handler)
